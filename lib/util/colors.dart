d98fb37fab07470c7b0671d5c0dcc0b0fbaa133a
OsileJesse123 <jesseosile@gmail.com>
2024-01-13 22:37:25 +0100

    app colors was added

---
+++ lib/util/colors.dart
@@ -0,0 +1,15 @@
+
+
+import 'dart:ui';
+
+const kStylingRed800 = Color(0xFFEA1800);
+const kStylingRed900 = Color(0xFFA5201A);
+const kStylingGrey200 = Color(0xFFE8E8E8);
+const kStylingBlack900 = Color(0xFF181818);
+const kStylingBlut400 = Color(0xFF6475E1);
+const kStylingGrey600 = Color(0xFF6C6C6C);
+const kStylingGrey100 = Color(0xFFF6F6F6);
+const kStylingBlue50 = Color(0xFFDBF8FA);
+const kStylingBlue800 = Color(0xFF0A828D);
+const kStylingGrey700 = Color(0xFF606060);
+const kStylingYellow600 = Color(0xFFE9C600);
