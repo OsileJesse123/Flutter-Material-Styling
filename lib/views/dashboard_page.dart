c550a80f37007ac50a56d4d65f15d167a3266574
OsileJesse123 <jesseosile@gmail.com>
2024-01-27 21:12:17 +0100

    doordash dashboard in progress

--- lib/views/dashboard_page.dart
+++ lib/views/dashboard_page.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
 
   @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+
+  final locations = <String>["dkkal", "kdajfa", "djkfajfa"];
+
+  @override
   Widget build(BuildContext context) {
+    String dropdownValue = locations.first;
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: Row(
+          children: [
+            Icon(
+              Icons.location_on,
+            ),
+            DropdownButton<String>(
+              value: dropdownValue,
+              items: locations.map((e) => DropdownMenuItem<String>(value: e,child: Text(e))).toList(),
+               onChanged: (value){
+                dropdownValue = value!;
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
